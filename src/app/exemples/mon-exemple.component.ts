4afe4b68fadc58dd7f1f34d80597a086e73e0c71
Alan CHOUFA <alan.choufa@agrosupdijon.fr>
2021-06-15 11:00:38 +0200

    feat: création liste élèves

--- src/app/exemples/mon-exemple.component.ts
+++ src/app/exemples/mon-exemple.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, NgModule, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import { groupBy } from 'lodash-es'
 
 
 @Component({
@@ -13,6 +14,18 @@ import {RouterModule, Routes} from '@angular/router';
 })
 export class MonExempleComponent implements OnInit {
 
+  /**
+   * On récupère tous les élèves de la 6ème
+   */
+  eleves = [
+    {class: '6A', name: 'John'},
+    {class: '6A', name: 'Marie'},
+    {class: '6A', name: 'Elise'},
+    {class: '6A', name: 'Bob'},
+    {class: '6B', name: 'Sophie'},
+    {class: '6B', name: 'Alexandre'},
+    {class: '6B', name: 'Romain'},
+  ];
 
   constructor() {
   }
